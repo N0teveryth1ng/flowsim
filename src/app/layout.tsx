449e47cb562917183e247100e5a9af0e55f4ca3c
S Das <dasw67352@gmail.com>
2026-09-25 10:43:49 +0530

    minot fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html lang="en" className="dark h-full antialiased">
       <body className="min-h-full flex flex-col bg-[#121316] text-white">{children}</body>
     </html>
   );
